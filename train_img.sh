f449ff0007d02c047d512b97ad4c09ef3fa08825
Xuanlei Zhao <xuanlei.zhao@gmail.com>
2024-02-21 09:10:16 +0000

    Remove data-path argument from train_img.sh

--- train_img.sh
+++ train_img.sh
@@ -1,1 +1,1 @@
-torchrun --standalone --nproc_per_node=2 train_img.py --model DiT-XL/2 --data-path ./processed --batch-size 1
+torchrun --standalone --nproc_per_node=2 train_img.py --model DiT-XL/2 --batch-size 1
