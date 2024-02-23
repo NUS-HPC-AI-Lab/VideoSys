ea1f83dbccedc8720fa8ea4d5f7af559f9a19f3a
Xuanlei Zhao <43881818+oahzxl@users.noreply.github.com>
2024-02-24 02:33:46 +0800

    hotfix accuracy and step num (#27)
    * fix
    
    * update settings
    
    * Update dtype to torch.float32 in DiT model
    
    * Remove unnecessary assert statement in DiT class
    
    * Refactor logging and checkpoint saving in train_img.py

--- train_img.py
+++ train_img.py
@@ -14,6 +14,7 @@ from glob import glob
 
 import colossalai
 import torch
+import torch.distributed as dist
 from colossalai.booster import Booster
 from colossalai.booster.plugin import LowLevelZeroPlugin
 from colossalai.cluster import DistCoordinator
@@ -60,6 +61,7 @@ def main(args):
     model_string_name = args.model.replace("/", "-")
     # Create an experiment folder
     experiment_dir = f"{args.outputs}/{experiment_index:03d}-{model_string_name}"
+    dist.barrier()
     if coordinator.is_master():
         os.makedirs(experiment_dir, exist_ok=True)
         with open(f"{experiment_dir}/config.txt", "w") as f:
@@ -113,6 +115,7 @@ def main(args):
             enable_layernorm_kernel=args.enable_layernorm_kernel,
             enable_modulate_kernel=args.enable_modulate_kernel,
             sequence_parallel_size=args.sequence_parallel_size,
+            dtype=dtype,
         )
         .to(device)
         .to(dtype)
@@ -208,7 +211,6 @@ def main(args):
                 with torch.no_grad():
                     # Map input images to latent space + normalize latents:
                     x = vae.encode(x).latent_dist.sample().mul_(0.18215)
-                    x = x.to(dtype)
 
                 # Diffusion
                 t = torch.randint(0, diffusion.num_timesteps, (x.shape[0],), device=device)
@@ -224,11 +226,15 @@ def main(args):
 
                 # Log loss values:
                 all_reduce_mean(loss)
-                if coordinator.is_master() and (step + 1) % args.log_every == 0:
-                    pbar.set_postfix({"loss": loss.item()})
-                    writer.add_scalar("loss", loss.item(), epoch * num_steps_per_epoch + step)
+                global_step = epoch * num_steps_per_epoch + step
+                pbar.set_postfix({"loss": loss.item(), "step": step, "global_step": global_step})
 
-                if args.ckpt_every > 0 and (step + 1) % args.ckpt_every == 0:
+                # Log to tensorboard
+                if coordinator.is_master() and (global_step + 1) % args.log_every == 0:
+                    writer.add_scalar("loss", loss.item(), global_step)
+
+                # Save checkpoint
+                if args.ckpt_every > 0 and (global_step + 1) % args.ckpt_every == 0:
                     logger.info(f"Saving checkpoint...")
                     save(
                         booster,
