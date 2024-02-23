f3f3afd8beaec07a78d46d81be5350d3f74c7346
KKZ20 <kanezz620@gmail.com>
2024-02-24 01:20:05 +0800

    polish code

--- opendit/utils/pg_utils.py
+++ opendit/utils/pg_utils.py
@@ -1,4 +1,6 @@
 from colossalai.cluster.process_group_mesh import ProcessGroupMesh
+from torch import nn
+from torch.distributed import ProcessGroup
 
 DP_AXIS = 0
 SP_AXIS = 1
@@ -7,3 +9,20 @@ SP_AXIS = 1
 class ProcessGroupManager(ProcessGroupMesh):
     def __init__(self, *size: int):
         super().__init__(*size)
+
+
+# Initialize a process group manager
+def initialize_process_group_maneger(world_size: int, sp_size: int) -> ProcessGroupManager:
+    assert world_size % sp_size == 0, f"World size {world_size} is not divisible by sequence parallel size {sp_size}"
+    dp_size = world_size // sp_size
+    pg_manager = ProcessGroupManager(dp_size, sp_size)
+    return pg_manager
+
+
+# Register sequence parallel group after copy
+def register_sequence_parallel_group(model: nn.Module, sp_group: ProcessGroup):
+    setattr(model, "sequence_parallel_group", sp_group)
+    for block in model.blocks:
+        setattr(block, "sequence_parallel_group", sp_group)
+        setattr(block.attn, "sequence_parallel_group", sp_group)
+    return model
--- train_img.sh
+++ train_img.sh
@@ -1,4 +1,4 @@
-torchrun --standalone --nproc_per_node=4 train_img.py \
+torchrun --standalone --nproc_per_node=2 train_img.py \
     --model DiT-XL/2 \
     --batch-size 2 \
     # --enable_modulate_kernel \
