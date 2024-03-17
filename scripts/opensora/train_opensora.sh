6945a6c558187edc637f13b8a7db6f37299b2e47
Xuanlei Zhao <xuanlei.zhao@gmail.com>
2024-03-17 21:29:26 +0800

    update train

--- scripts/opensora/train_opensora.sh
+++ scripts/opensora/train_opensora.sh
@@ -1,7 +1,7 @@
 BATCH_SIZE=2
 LR=2e-5
-DATA_PATH="/home/zhaowangbo/data_hdd/csv/inter4k_pexels_rp_fmin_48.csv"
-MODEL_PRETRAINED_PATH="/home/zhaowangbo/zangwei/minisora_new/outputs/314-F16S3-PixArt-ST-XL-2/epoch128-global_step20000/ema.pt"
+DATA_PATH="csv_path"
+MODEL_PRETRAINED_PATH="model_path"
 
 torchrun --standalone --nproc_per_node=2 train_opensora.py \
     --batch_size $BATCH_SIZE \
