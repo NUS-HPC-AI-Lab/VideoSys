8b5560992090363dd7e9f2f03d012d38eeb6e44c
Xuanlei Zhao <xuanlei.zhao@gmail.com>
2024-03-18 09:59:09 +0800

    update dit scripts

--- scripts/dit/sample_dit.sh
+++ scripts/dit/sample_dit.sh
@@ -1,4 +1,4 @@
-python sample_dit.py \
+python scripts/dit/sample_dit.py \
     --model DiT-XL/2 \
     --image_size 256 \
     --num_classes 1000 \
--- scripts/dit/train_dit.sh
+++ scripts/dit/train_dit.sh
@@ -1,10 +1,10 @@
-torchrun --standalone --nproc_per_node=2 train_dit.py \
+torchrun --standalone --nproc_per_node=2 scripts/dit/train_dit.py \
     --model DiT-XL/2 \
     --batch_size 2 \
     --num_classes 10
 
 # recommend setting
-# torchrun --standalone --nproc_per_node=8 train_dit.py \
+# torchrun --standalone --nproc_per_node=8 scripts/dit/train_dit.py \
 #     --model DiT-XL/2 \
 #     --batch_size 180 \
 #     --enable_layernorm_kernel \
