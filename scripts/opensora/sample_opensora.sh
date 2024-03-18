54e4efc480e14e42a7ab2b8c592e6e3a51476c6d
Xuanlei Zhao <xuanlei.zhao@gmail.com>
2024-03-18 10:33:47 +0800

    update opensora infer script

--- scripts/opensora/sample_opensora.sh
+++ scripts/opensora/sample_opensora.sh
@@ -1,14 +1,13 @@
-python sample_opensora.py \
+python scripts/opensora/sample_opensora.py \
     --model_time_scale 1 \
     --model_space_scale 1 \
     --image_size 512 512 \
     --num_frames 16 \
     --fps 8 \
-    --dtype fp16 \
-    --model_pretrained_path ckpt_path
+    --dtype fp16
 
 # recommend setting
-# python sample_opensora.py \
+# python scripts/opensora/sample_opensora.py \
 #     --model_time_scale 1 \
 #     --model_space_scale 1 \
 #     --image_size 512 512 \
