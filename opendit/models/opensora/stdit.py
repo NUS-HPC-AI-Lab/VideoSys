28331e7260433714ca5c13b1ae37c9d16717dbc5
Xuanlei Zhao <xuanlei.zhao@gmail.com>
2024-05-30 12:42:05 -0700

    update ref

--- opendit/models/opensora/stdit.py
+++ opendit/models/opensora/stdit.py
@@ -1,10 +1,9 @@
-# Adapted from OpenSora and DiT
+# Adapted from OpenSora
 
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 # --------------------------------------------------------
 # References:
-# DiT:      https://github.com/facebookresearch/DiT
 # OpenSora: https://github.com/hpcaitech/Open-Sora
 # --------------------------------------------------------
 
