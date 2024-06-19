4663078ecff743ecf6b96287c70dbe4629bfba91
Xuanlei Zhao <43881818+oahzxl@users.noreply.github.com>
2024-06-19 13:34:58 -0700

    Opensorav1.2 (#11)
    * add opensorav1.2
    
    * update final
    
    * udpate mgr

--- opendit/utils/utils.py
+++ opendit/utils/utils.py
@@ -1,8 +1,10 @@
+import logging
 import os
 import random
 
 import numpy as np
 import torch
+import torch.distributed as dist
 from omegaconf import DictConfig, ListConfig, OmegaConf
 
 
@@ -50,3 +52,43 @@ def merge_args(args1, args2):
             raise RuntimeError(f"Unknown argument {k}")
 
     return args1
+
+
+def is_distributed():
+    return os.environ.get("WORLD_SIZE", None) is not None
+
+
+def is_main_process():
+    return not is_distributed() or dist.get_rank() == 0
+
+
+def all_exists(paths):
+    return all(os.path.exists(path) for path in paths)
+
+
+def get_logger():
+    return logging.getLogger(__name__)
+
+
+def create_logger(logging_dir=None):
+    """
+    Create a logger that writes to a log file and stdout.
+    """
+    if is_main_process():  # real logger
+        additional_args = dict()
+        if logging_dir is not None:
+            additional_args["handlers"] = [
+                logging.StreamHandler(),
+                logging.FileHandler(f"{logging_dir}/log.txt"),
+            ]
+        logging.basicConfig(
+            level=logging.INFO,
+            format="[\033[34m%(asctime)s\033[0m] %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+            **additional_args,
+        )
+        logger = logging.getLogger(__name__)
+    else:  # dummy logger (does nothing)
+        logger = logging.getLogger(__name__)
+        logger.addHandler(logging.NullHandler())
+    return logger
