314dce8e4ece6b0447fde24016782d2ec237972f
Jinxiaolong1129 <inxiaolong1129@gmail.com>
2024-06-20 22:16:05 -0400

    final code clean

--- scripts/opensora_plan/sample_opensora_plan.sh
+++ scripts/opensora_plan/sample_opensora_plan.sh
@@ -1,6 +1,6 @@
 python scripts/opensora_plan/sample_opensora_plan.py --config configs/opensora_plan/sample_65f.yaml
 
-export CUDA_VISIBLE_DEVICES=4
+export CUDA_VISIBLE_DEVICES=3
 python scripts/opensora_plan/sample_opensora_plan.py --config configs/opensora_plan/sample_65f_skip.yaml
 
 # python scripts/opensora_plan/sample_opensora_plan.py --config configs/opensora_plan/sample_221f.yaml
