ff3805b65c665dc065fcc8dcfdbc55739612f4b7
Jinxiaolong1129 <inxiaolong1129@gmail.com>
2024-07-02 22:12:48 -0400

    update

--- scripts/opensora_plan/sample_opensora_plan_skip_s_t.sh
+++ scripts/opensora_plan/sample_opensora_plan_skip_s_t.sh
@@ -10,3 +10,9 @@ echo "Finished opensora_plan_sample_65f_skip_s_t_3.txt"
 
 CUDA_VISIBLE_DEVICES=7 torchrun --standalone --nproc_per_node=1 scripts/opensora_plan/sample_opensora_plan_skip_s_t.py --config configs/opensora_plan/sample_65f_skip_s_t_4.yaml > opensora_plan_sample_65f_skip_s_t_4.txt 2>&1 &
 echo "Finished opensora_plan_sample_65f_skip_s_t_4.txt"
+
+CUDA_VISIBLE_DEVICES=6 torchrun --standalone --nproc_per_node=1 scripts/opensora_plan/sample_opensora_plan_skip_s_t.py --config configs/opensora_plan/sample_65f_skip_s_t_5.yaml > opensora_plan_sample_65f_skip_s_t_5.txt 2>&1 &
+echo "Finished opensora_plan_sample_65f_skip_s_t_3.txt"
+
+CUDA_VISIBLE_DEVICES=7 torchrun --standalone --nproc_per_node=1 scripts/opensora_plan/sample_opensora_plan_skip_s_t.py --config configs/opensora_plan/sample_65f_skip_s_t_6.yaml > opensora_plan_sample_65f_skip_s_t_6.txt 2>&1 &
+echo "Finished opensora_plan_sample_65f_skip_s_t_4.txt"
