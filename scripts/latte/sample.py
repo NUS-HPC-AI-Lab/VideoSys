3596f654c42359ac4598bb699b737be949c8c050
Xuanlei Zhao <xuanlei.zhao@gmail.com>
2024-07-15 05:52:46 +0000

    update

--- scripts/latte/sample.py
+++ scripts/latte/sample.py
@@ -8,4 +8,4 @@ pipeline = LattePipeline(config)
 
 prompt = "Sunset over the sea."
 video = pipeline(prompt).video[0]
-pipeline.save_video(video, f"./outputs/video_{prompt[:50]}.mp4")
+pipeline.save_video(video, f"./outputs/{prompt[:50]}.mp4")
