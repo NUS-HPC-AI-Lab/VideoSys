fe0b1235f1790f45cf231b60fd17ea9fa471d075
Xuanlei Zhao <xuanlei.zhao@gmail.com>
2024-07-30 22:58:59 +0000

    update vbench

--- eval/vbench/run_vbench.py
+++ eval/vbench/run_vbench.py
@@ -1,15 +1,34 @@
 import torch
 from vbench import VBench
 
-full_info_path = "/data/xuanlei/Lumiere-exp/eval/vbench/VBench_test.json"
+full_info_path = "./vbench/VBench_test.json"
 
+dimensions = [
+    "subject_consistency",
+    "imaging_quality",
+    "background_consistency",
+    "motion_smoothness",
+    "overall_consistency",
+    "human_action",
+    "multiple_objects",
+    "spatial_relationship",
+    "object_class",
+    "color",
+    "aesthetic_quality",
+    "appearance_style",
+    "temporal_flickering",
+    "scene",
+    "temporal_style",
+    "dynamic_degree",
+]
 
-my_VBench = VBench(torch.device("cuda"), full_info_path, "vbench_out")
-my_VBench.evaluate(
-    videos_path="/data/xuanlei/Lumiere-exp/eval/videos",
-    name="temporal_flickering",
-    local=False,
-    read_frame=False,
-    dimension_list=["temporal_flickering"],
-    mode="vbench_standard",
-)
+for dimension in dimensions:
+    my_VBench = VBench(torch.device("cuda"), full_info_path, "vbench_out")
+    my_VBench.evaluate(
+        videos_path="./videos",
+        name=dimension,
+        local=False,
+        read_frame=False,
+        dimension_list=[dimension],
+        mode="vbench_standard",
+    )
