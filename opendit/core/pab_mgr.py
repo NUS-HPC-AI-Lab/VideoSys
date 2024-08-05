994a348e0aefaeb3bae13d5f001943fe1a332350
Jxl <jinxiaolong1129@gmail.com>
2024-08-04 21:04:34 -0400

    implement mlp broadcast (#150)
    * init commit (#3)
    
    * update diffusion
    
    * polish
    
    * update structure
    
    * embed
    
    * update ref
    
    * update ref
    
    * udpate opensora init
    
    * remove
    
    * update
    
    * update name
    
    * fix v1
    
    * update ignore
    
    * update stdit2
    
    * update v2 infer
    
    * support v2 infer
    
    * update script
    
    * polish code
    
    * update script
    
    * update script and rename
    
    * fix cross
    
    * update code for temporal and cross
    
    * update base script
    
    * remove layernorm for infer
    
    * update
    
    * add readme (#4)
    
    * update default setting (#5)
    
    * Spatial attention (#6)
    
    * update prompt
    
    * update spatial
    
    * update code control
    
    * Merge (#7)
    
    * update diffusion
    
    * polish
    
    * update structure
    
    * embed
    
    * update ref
    
    * update ref
    
    * udpate opensora init
    
    * remove
    
    * update
    
    * update name
    
    * fix v1
    
    * update ignore
    
    * update stdit2
    
    * update v2 infer
    
    * support v2 infer
    
    * update script
    
    * polish code
    
    * update script
    
    * update script and rename
    
    * fix cross
    
    * fix some bugs
    
    * optimize attn
    
    * add latte
    
    * update
    
    * update opensora sample
    
    * remove useless arg
    
    * Update save_dir path in opensora sample configs
    
    * update test
    
    * seed
    
    * update opensora plan
    
    * update opensora plan
    
    * update yaml
    
    * remove useless
    
    * update opensora plan
    
    * update doc
    
    * update script
    
    * update data
    
    * update code
    
    * update usage
    
    * update
    
    * add attn skip for latte and opensoraplan (#8)
    
    * add block
    
    * fix
    
    * update latte
    
    * update
    
    * add latte args (#9)
    
    * fix latte argparse (#10)
    
    * add kw
    
    * fix arg
    
    * add eval
    
    * update ignore
    
    * Opensorav1.2 (#11)
    
    * add opensorav1.2
    
    * update final
    
    * udpate mgr
    
    * support opensora skip (#12)
    
    * update opensora yaml
    
    * add shard
    
    * update opensora skip
    
    * update config for other models (#13)
    
    * finish opensora-plan and latte
    
    * Sp (#14)
    
    * update latte shard
    
    * rename arg
    
    * update license
    
    * update lincense file
    
    * update dynamic sp for opensoraplan and latte
    
    * Update STDiT3Block and STDiT3 classes in stdit3.py
    
    * finish opensora
    
    * code refine
    
    * code refine
    
    * final code clean
    
    * update skip_diffusion_timestep function to handle more than 1 step skip
    
    * update skip config (#16)
    
    * udpate range for all
    
    * update opensora plan
    
    * update code
    
    * polish code
    
    * polish print
    
    * update default value
    
    * update verbose
    
    * finish latte eval generate function
    
    * update gitignore
    
    * finish eval
    TODO:
    BUG check whether center crop is correct
    TODO original video size< generated videos, how to resize?
    
    * update eval save
    
    * update mlp skip
    
    * update
    
    * parse mlp skip
    
    * update
    
    * update
    
    * update
    
    * update test mlp mse
    
    * update
    
    * update
    
    * update
    
    * update skip s t
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update latte
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update finish skip function
    
    * delete and change file name to clean code
    
    * update
    
    * update
    
    * clean code finish
    
    * delete mse code and clean code finish
    
    * delete and clean code
    
    * clean eval code
    
    * delete asset
    
    * update
    
    * polish code
    
    * polish
    
    * update rflow
    
    ---------
    
    Co-authored-by: Xuanlei Zhao <xuanlei.zhao@gmail.com>
    Co-authored-by: Jinxiaolong1129 <inxiaolong1129@gmail.com>

--- opendit/core/pab_mgr.py
+++ opendit/core/pab_mgr.py
@@ -24,6 +24,9 @@ class PABConfig:
         diffusion_skip: bool,
         diffusion_timestep_respacing: list,
         diffusion_skip_timestep: list,
+        mlp_skip: bool,
+        mlp_spatial_skip_config: dict,
+        mlp_temporal_skip_config: dict,
     ):
         self.steps = steps
 
@@ -42,6 +45,13 @@ class PABConfig:
         self.diffusion_skip = diffusion_skip
         self.diffusion_timestep_respacing = diffusion_timestep_respacing
         self.diffusion_skip_timestep = diffusion_skip_timestep
+
+        self.mlp_skip = mlp_skip
+        self.mlp_spatial_skip_config = mlp_spatial_skip_config
+        self.mlp_temporal_skip_config = mlp_temporal_skip_config
+
+        self.temporal_mlp_outputs = {}
+        self.spatial_mlp_outputs = {}
 
 
 class PABManager:
@@ -93,6 +103,89 @@ class PABManager:
         count = (count + 1) % self.config.steps
         return flag, count
 
+    @staticmethod
+    def _is_t_in_skip_config(all_timesteps, timestep, config):
+        is_t_in_skip_config = False
+        for key in config:
+            index = all_timesteps.index(key)
+            skip_range = all_timesteps[index : index + 1 + int(config[key]["skip_count"])]
+            if timestep in skip_range:
+                is_t_in_skip_config = True
+                skip_range = [all_timesteps[index], all_timesteps[index + int(config[key]["skip_count"])]]
+                break
+        return is_t_in_skip_config, skip_range
+
+    def if_skip_mlp(self, timestep: int, count: int, block_idx: int, all_timesteps, is_temporal=False):
+        if is_temporal:
+            cur_config = self.config.mlp_temporal_skip_config
+        else:
+            cur_config = self.config.mlp_spatial_skip_config
+
+        is_t_in_skip_config, skip_range = self._is_t_in_skip_config(all_timesteps, timestep, cur_config)
+        next_flag = False
+        if (
+            self.config.mlp_skip
+            and (timestep is not None)
+            and (timestep in cur_config)
+            and (block_idx in cur_config[timestep]["block"])
+        ):
+            flag = False
+            next_flag = True
+            count = count + 1
+        elif (
+            self.config.mlp_skip
+            and (timestep is not None)
+            and (is_t_in_skip_config)
+            and (block_idx in cur_config[skip_range[0]]["block"])
+        ):
+            flag = True
+            count = 0
+        else:
+            flag = False
+
+        return flag, count, next_flag, skip_range
+
+    def save_skip_output(self, timestep, block_idx, ff_output, is_temporal=False):
+        if is_temporal:
+            self.config.temporal_mlp_outputs[(timestep, block_idx)] = ff_output
+        else:
+            self.config.spatial_mlp_outputs[(timestep, block_idx)] = ff_output
+
+    def get_mlp_output(self, skip_range, timestep, block_idx, is_temporal=False):
+        skip_start_t = skip_range[0]
+        if is_temporal:
+            skip_output = (
+                self.config.temporal_mlp_outputs.get((skip_start_t, block_idx), None)
+                if self.config.temporal_mlp_outputs is not None
+                else None
+            )
+        else:
+            skip_output = (
+                self.config.spatial_mlp_outputs.get((skip_start_t, block_idx), None)
+                if self.config.spatial_mlp_outputs is not None
+                else None
+            )
+
+        if skip_output is not None:
+            if timestep == skip_range[-1]:
+                # TODO: save memory
+                if is_temporal:
+                    del self.config.temporal_mlp_outputs[(skip_start_t, block_idx)]
+                else:
+                    del self.config.spatial_mlp_outputs[(skip_start_t, block_idx)]
+        else:
+            raise ValueError(
+                f"No stored MLP output found | t {timestep} |[{skip_range[0]}, {skip_range[-1]}] | block {block_idx}"
+            )
+
+        return skip_output
+
+    def get_spatial_mlp_outputs(self):
+        return self.config.spatial_mlp_outputs
+
+    def get_temporal_mlp_outputs(self):
+        return self.config.temporal_mlp_outputs
+
 
 def set_pab_manager(config: PABConfig):
     global PAB_MANAGER
@@ -130,6 +223,20 @@ def if_broadcast_spatial(timestep: int, count: int, block_idx: int):
     if not enable_pab():
         return False, count
     return PAB_MANAGER.if_broadcast_spatial(timestep, count, block_idx)
+
+
+def if_broadcast_mlp(timestep: int, count: int, block_idx: int, all_timesteps, is_temporal=False):
+    if not enable_pab():
+        return False, count
+    return PAB_MANAGER.if_skip_mlp(timestep, count, block_idx, all_timesteps, is_temporal)
+
+
+def save_mlp_output(timestep: int, block_idx: int, ff_output, is_temporal=False):
+    return PAB_MANAGER.save_skip_output(timestep, block_idx, ff_output, is_temporal)
+
+
+def get_mlp_output(skip_range, timestep, block_idx: int, is_temporal=False):
+    return PAB_MANAGER.get_mlp_output(skip_range, timestep, block_idx, is_temporal)
 
 
 def get_diffusion_skip():
