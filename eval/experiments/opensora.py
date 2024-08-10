84b0de23275a8d5628c932b23bb1bd403c1e1649
Xuanlei Zhao <xuanlei.zhao@gmail.com>
2024-08-10 08:57:13 +0000

    update loop

--- eval/experiments/opensora.py
+++ eval/experiments/opensora.py
@@ -8,14 +8,14 @@ def eval_base(prompt_list):
     config = OpenSoraConfig()
     pipeline = OpenSoraPipeline(config)
 
-    generate_func(pipeline, prompt_list, "./samples/opensora_base", loop=1)
+    generate_func(pipeline, prompt_list, "./samples/opensora_base", loop=5)
 
 
 def eval_pab(prompt_list):
     config = OpenSoraConfig(enable_pab=True)
     pipeline = OpenSoraPipeline(config)
 
-    generate_func(pipeline, prompt_list, "./samples/opensora_pab", loop=1)
+    generate_func(pipeline, prompt_list, "./samples/opensora_pab", loop=5)
 
 
 if __name__ == "__main__":
