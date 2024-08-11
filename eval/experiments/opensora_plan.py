ee49d4082a536108ee0e3b3d3bbf35d22b3a96ed
Xuanlei Zhao <xuanlei.zhao@gmail.com>
2024-08-11 02:20:49 -0700

    fix typo

--- eval/experiments/opensora_plan.py
+++ eval/experiments/opensora_plan.py
@@ -16,7 +16,7 @@ def eval_pab1(prompt_list):
     pab_config = OpenSoraPlanPABConfig(
         spatial_gap=2,
         temporal_gap=4,
-        spatial_size=6,
+        cross_gap=6,
     )
     config = OpenSoraPlanConfig(enable_pab=True, pab_config=pab_config)
     pipeline = OpenSoraPlanPipeline(config)
@@ -28,7 +28,7 @@ def eval_pab2(prompt_list):
     pab_config = OpenSoraPlanPABConfig(
         spatial_gap=3,
         temporal_gap=5,
-        spatial_size=7,
+        cross_gap=7,
     )
     config = OpenSoraPlanConfig(enable_pab=True, pab_config=pab_config)
     pipeline = OpenSoraPlanPipeline(config)
@@ -40,7 +40,7 @@ def eval_pab3(prompt_list):
     pab_config = OpenSoraPlanPABConfig(
         spatial_gap=5,
         temporal_gap=7,
-        spatial_size=9,
+        cross_gap=9,
     )
     config = OpenSoraPlanConfig(enable_pab=True, pab_config=pab_config)
     pipeline = OpenSoraPlanPipeline(config)
@@ -51,7 +51,7 @@ def eval_pab3(prompt_list):
 if __name__ == "__main__":
     opendit.initialize(42)
     prompt_list = read_prompt_list("vbench/VBench_full_info.json")
-    eval_base(prompt_list)
-    eval_pab1(prompt_list)
+    # eval_base(prompt_list)
+    # eval_pab1(prompt_list)
     eval_pab2(prompt_list)
     eval_pab3(prompt_list)
