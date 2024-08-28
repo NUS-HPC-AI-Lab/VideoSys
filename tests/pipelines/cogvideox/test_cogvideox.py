2e1bc266ca5ab23a56e09e7ec5637918bae47dc0
Xuanlei Zhao <xuanlei.zhao@gmail.com>
2024-08-28 17:09:44 +0000

    update cogvideox test

--- tests/pipelines/cogvideox/test_cogvideox.py
+++ tests/pipelines/cogvideox/test_cogvideox.py
@@ -11,3 +11,13 @@ def test_base(world_size):
     prompt = "Sunset over the sea."
     video = engine.generate(prompt).video[0]
     engine.save_video(video, f"./test_outputs/{prompt}_cogvideo_{world_size}.mp4")
+
+
+@pytest.mark.parametrize("world_size", [1])
+def test_pab(world_size):
+    config = CogVideoConfig(world_size=world_size, enable_pab=True)
+    engine = VideoSysEngine(config)
+
+    prompt = "Sunset over the sea."
+    video = engine.generate(prompt).video[0]
+    engine.save_video(video, f"./test_outputs/{prompt}_cogvideo_pab_{world_size}.mp4")
