57718e18452d96f0dfc48e00f57d529377695a7f
Xuanlei Zhao <xuanlei.zhao@gmail.com>
2024-08-29 22:41:00 +0000

    update tests

--- tests/pipelines/cogvideox/test_cogvideox.py
+++ tests/pipelines/cogvideox/test_cogvideox.py
@@ -3,21 +3,21 @@ import pytest
 from videosys import CogVideoXConfig, VideoSysEngine
 
 
-@pytest.mark.parametrize("world_size", [1])
-def test_base(world_size):
-    config = CogVideoXConfig(num_gpus=world_size)
+@pytest.mark.parametrize("num_gpus", [1])
+def test_base(num_gpus):
+    config = CogVideoXConfig(num_gpus=num_gpus)
     engine = VideoSysEngine(config)
 
     prompt = "Sunset over the sea."
     video = engine.generate(prompt).video[0]
-    engine.save_video(video, f"./test_outputs/{prompt}_cogvideo_{world_size}.mp4")
+    engine.save_video(video, f"./test_outputs/{prompt}_cogvideo_{num_gpus}.mp4")
 
 
-@pytest.mark.parametrize("world_size", [1])
-def test_pab(world_size):
-    config = CogVideoXConfig(num_gpus=world_size, enable_pab=True)
+@pytest.mark.parametrize("num_gpus", [1])
+def test_pab(num_gpus):
+    config = CogVideoXConfig(num_gpus=num_gpus, enable_pab=True)
     engine = VideoSysEngine(config)
 
     prompt = "Sunset over the sea."
     video = engine.generate(prompt).video[0]
-    engine.save_video(video, f"./test_outputs/{prompt}_cogvideo_pab_{world_size}.mp4")
+    engine.save_video(video, f"./test_outputs/{prompt}_cogvideo_pab_{num_gpus}.mp4")
--- tests/pipelines/open_sora_plan/test_open_sora_plan.py
+++ tests/pipelines/open_sora_plan/test_open_sora_plan.py
@@ -3,21 +3,21 @@ import pytest
 from videosys import OpenSoraPlanConfig, VideoSysEngine
 
 
-@pytest.mark.parametrize("world_size", [1, 2])
-def test_base(world_size):
-    config = OpenSoraPlanConfig(num_gpus=world_size)
+@pytest.mark.parametrize("num_gpus", [1, 2])
+def test_base(num_gpus):
+    config = OpenSoraPlanConfig(num_gpus=num_gpus)
     engine = VideoSysEngine(config)
 
     prompt = "Sunset over the sea."
     video = engine.generate(prompt).video[0]
-    engine.save_video(video, f"./test_outputs/{prompt}_open_sora_pab_{world_size}.mp4")
+    engine.save_video(video, f"./test_outputs/{prompt}_open_sora_pab_{num_gpus}.mp4")
 
 
-@pytest.mark.parametrize("world_size", [1, 2])
-def test_pab(world_size):
-    config = OpenSoraPlanConfig(num_gpus=world_size)
+@pytest.mark.parametrize("num_gpus", [1, 2])
+def test_pab(num_gpus):
+    config = OpenSoraPlanConfig(num_gpus=num_gpus)
     engine = VideoSysEngine(config)
 
     prompt = "Sunset over the sea."
     video = engine.generate(prompt).video[0]
-    engine.save_video(video, f"./test_outputs/{prompt}_open_sora_plan_pab_{world_size}.mp4")
+    engine.save_video(video, f"./test_outputs/{prompt}_open_sora_plan_pab_{num_gpus}.mp4")
