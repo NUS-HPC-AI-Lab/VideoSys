ca53cf5e974dc7100059a8609623d5d223a4ed72
Xuanlei Zhao <xuanlei.zhao@gmail.com>
2024-12-10 21:34:08 +0800

    update

--- examples/training/open_sora/benchmark.sh
+++ examples/training/open_sora/benchmark.sh
@@ -1,17 +1,12 @@
 # =============== environment variables ================
-# used by torch.distributed
-export NNODES=1
-export NODE_RANK=0
-export GPUS_PER_NODE=8
-export WORLD_SIZE=$(($NNODES*$GPUS_PER_NODE))
-export MASTER_ADDR="localhost"
-export MASTER_PORT=29502
-
 export PYTHONPATH=$PYTHONPATH:$PWD
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 export TOKENIZERS_PARALLELISM=false
 
-# =============== launch commands ================
+# =============== benchmark commands ================
+# If you run this for the first time, you need to run the program twice.
+# The first time is to profile the model, and the second time is to run the benchmark.
+
 # baseline
 torchrun --standalone --nproc_per_node 8 examples/training/open_sora/train.py \
     examples/training/open_sora/configs/benchmarks/baseline.yaml > baseline.log 2>& 1
